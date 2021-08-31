b4c3682edd54ab28a0cbb3a0a964a0e2e606903d
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-31 15:33:05 -0300

    feat: create migrations

---
+++ database/migrations/1630428460654_tasks.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Tasks extends BaseSchema {
+  protected tableName = 'tasks'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title')
+      table.string('description')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
